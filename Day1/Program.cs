9c90bbae7d3c6e7429314f83ec1a5c6f68f27a91
Nikhil Jamuda <142397954+nimx89@users.noreply.github.com>
2024-12-03 17:59:03 +0530

    Day 1 and Day 3 Added

---
+++ Day1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Day_1;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1;
+        int[] array2;
+        try
+        {
+            string filename = "day1.txt";
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(),filename);
+            
+            if (!File.Exists(filePath)){
+                Console.WriteLine($"File '{filename} does not exits in the current directory.");
+                return;
+            }
+
+            List<int> column1 = new List<int>();
+            List<int> column2 = new List<int>();
+
+            foreach(string line in File.ReadAllLines(filePath)){
+                string[] parts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                
+                if (int.TryParse(parts[0],out int num1) && int.TryParse(parts[1],out int num2)){
+                    column1.Add(num1);
+                    column2.Add(num2);
+                }
+                else{
+                    Console.WriteLine($"Invalid number foramt");
+                }
+            }
+            array1 = column1.ToArray();
+            array2 = column2.ToArray();
+            Array.Sort(array1);
+            Array.Sort(array2); 
+            int sum =0 ;
+            int occur = 0;
+            int len = array1.Length;
+            int j = 0;
+
+            // part 1 starts
+            // uncomment this for part 1
+            // for(int i = 0; i<len;i++){
+            //     sum += Math.Abs(array1[i]-array2[i]);
+            // }
+            //part 1 ends
+
+            //part 2 starts
+            // comment this for part 2
+            for (int i = 0; i < len; i++)
+            {
+                while(j<len && array1[i]>=array2[j]){
+                    if (array1[i] == array2[j]){
+                        occur++;
+                    }
+                    j++;
+                }
+                sum = sum +array1[i]*occur;
+                occur = 0;
+                j=0;
+            }
+            //part 2 ends
+
+
+            Console.WriteLine("Sum = "+sum);
+
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("error");
+            throw;
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Day3;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string filename = "input.txt"; ;
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(),filename);
+            if(!File.Exists(filePath)){
+                Console.WriteLine($"File '{filename}' does not exits in the current directory. " ); 
+                return;
+            }
+
+            bool multiply = true; // flag to check if we need to multiply or not, it is outside so that after a line the flag must remain same
+
+            int sum1 = 0; // sum for part 1
+            int sum2 = 0; // sum for part 2
+            
+            string pattern1 = @"mul\(\d{1,3},\d{1,3}\)"; // pattern for regex for part 1
+            string pattern2 =  @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)"; // pattern for regex for part 2
+
+            foreach(string line in File.ReadAllLines(filePath)){
+
+                //for part 1
+                MatchCollection parts1 = Regex.Matches(line,pattern1);
+                foreach(Match part in parts1){
+                    string[] numbers = part.Value.Replace("mul(","").Replace(")","").Split(","); //extracting numbers from the string
+                    int num1 = int.Parse(numbers[0]);
+                    int num2 = int.Parse(numbers[1]);
+                    int prd = num1 * num2;
+                    sum1 += prd;
+
+                }
+
+                //for part 2
+                MatchCollection parts2 = Regex.Matches(line,pattern2);
+                foreach(Match part in parts2){
+                    if(part.Value == "do()"){ // if we get do() then we need to multiply
+                        multiply = true;
+                    }
+                    else if(part.Value == "don't()"){ // if we get don't() then we don't need to multiply
+                        multiply = false;
+                    }
+                    else if(multiply){
+                        string[] numbers = part.Value.Replace("mul(","").Replace(")","").Split(",");
+                        int num1 = int.Parse(numbers[0]);
+                        int num2 = int.Parse(numbers[1]);
+                        int prd = num1 * num2;
+                        sum2 += prd;
+                        
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"Part 1 :{sum1}");
+            Console.WriteLine($"Part 2 :{sum2}");
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Some error occured");
+            throw;
+        }
+    }
+}
